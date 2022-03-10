bca66f390c9189eeaa2077c28b39686a32af1897
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-03-09 23:41:35 -0800

    Fix file path

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import datafusion
 
 if __name__ == "__main__":
     ctx = datafusion.ExecutionContext()
-    ctx.register_parquet("lineitem", "/tmp/tpch_sf1_parquet/lineitem/lineitem.parquet")
-    ctx.register_parquet("orders", "/tmp/tpch_sf1_parquet/orders/orders.parquet")
-    ctx.register_parquet("customer", "/tmp/tpch_sf1_parquet/customer/customer.parquet")
-    ctx.register_parquet("partsupp", "/tmp/tpch_sf1_parquet/partsupp/partsupp.parquet")
-    ctx.register_parquet("part", "/tmp/tpch_sf1_parquet/part/part.parquet")
-    ctx.register_parquet("nation", "/tmp/tpch_sf1_parquet/nation/nation.parquet")
-    ctx.register_parquet("region", "/tmp/tpch_sf1_parquet/region/region.parquet")
-    ctx.register_parquet("supplier", "/tmp/tpch_sf1_parquet/supplier/supploer.parquet")
+    ctx.register_parquet("lineitem", "/tmp/tpch_sf1_parquet/lineitem/lineitem.tbl.parquet")
+    ctx.register_parquet("orders", "/tmp/tpch_sf1_parquet/orders/orders.tbl.parquet")
+    ctx.register_parquet("customer", "/tmp/tpch_sf1_parquet/customer/customer.tbl.parquet")
+    ctx.register_parquet("partsupp", "/tmp/tpch_sf1_parquet/partsupp/partsupp.tbl.parquet")
+    ctx.register_parquet("part", "/tmp/tpch_sf1_parquet/part/part.tbl.parquet")
+    ctx.register_parquet("nation", "/tmp/tpch_sf1_parquet/nation/nation.tbl.parquet")
+    ctx.register_parquet("region", "/tmp/tpch_sf1_parquet/region/region.tbl.parquet")
+    ctx.register_parquet("supplier", "/tmp/tpch_sf1_parquet/supplier/supploer.tbl.parquet")
 
 
     query = """
