c8cdd91555746c71e6cc5b725b97ddf2b6ae7c51
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-03-10 13:12:56 -0800

    Fix query 4

--- queries/q4.sql
+++ queries/q4.sql
@@ -1,20 +1,21 @@
-SELECT
+select
     o_orderpriority,
-    count(*) AS order_count
-FROM
+    count(*) as order_count
+from
     orders
-WHERE
-    o_orderdate >= CAST('1993-07-01' AS date)
-    AND o_orderdate < CAST('1993-10-01' AS date)
-    AND EXISTS (
-        SELECT
+where
+        o_orderdate >= '1993-07-01'
+  and o_orderdate < date '1993-07-01' + interval '3' month
+  and exists (
+        select
             *
-        FROM
+        from
             lineitem
-        WHERE
-            l_orderkey = o_orderkey
-            AND l_commitdate < l_receiptdate)
-GROUP BY
+        where
+                l_orderkey = o_orderkey
+          and l_commitdate < l_receiptdate
+    )
+group by
     o_orderpriority
-ORDER BY
+order by
     o_orderpriority;
