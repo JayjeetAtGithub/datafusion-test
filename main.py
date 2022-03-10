bb5684a682b738c05cfcd45a44305c39848bd43d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-03-10 09:10:48 -0800

    Add TPCH queries

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pyarrow
 import datafusion
 
@@ -13,40 +14,12 @@ if __name__ == "__main__":
     ctx.register_parquet("supplier", "/tmp/tpch_sf1_parquet/supplier/supplier.tbl.parquet")
 
 
-    query = """
-SELECT
-    c_custkey,
-    c_name,
-    sum(l_extendedprice * (1 - l_discount)) AS revenue,
-    c_acctbal,
-    n_name,
-    c_address,
-    c_phone,
-    c_comment
-FROM
-    customer,
-    orders,
-    lineitem,
-    nation
-WHERE
-    c_custkey = o_custkey
-    AND l_orderkey = o_orderkey
-    AND o_orderdate >= CAST('1993-10-01' AS date)
-    AND o_orderdate < CAST('1994-01-01' AS date)
-    AND l_returnflag = 'R'
-    AND c_nationkey = n_nationkey
-GROUP BY
-    c_custkey,
-    c_name,
-    c_acctbal,
-    c_phone,
-    n_name,
-    c_address,
-    c_comment
-ORDER BY
-    revenue DESC
-LIMIT 20;
-    """
 
-    batches = ctx.sql(query).collect()
-    print(batches)
+    querie_files = os.listdir("queries")
+    for query_file in querie_files:
+        with open("queries/" + query_file, "r") as f:
+            query = f.read()
+        print("Executing query: " + query_file)
+        result = ctx.sql(query).collect()
+        print(result)
+        print("\n")
